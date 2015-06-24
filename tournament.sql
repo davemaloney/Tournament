3ba295fd5fc21e4fc6ed824a61a36fcedbf2e5a2
Dave Maloney <david.s.maloney@gmail.com>
2015-06-23 20:09:14 -0400

    Readability updates
    Attempted to make code more standard; added database creation command.

--- tournament.sql
+++ tournament.sql
@@ -6,60 +6,64 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-create table players (
+DROP DATABASE if exists tournament;
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE players (
        id serial PRIMARY KEY,
        name text);
 
-create table matches (
+CREATE TABLE matches (
        matchid serial PRIMARY KEY,
 --       round integer default 1,
-       winner integer references players(id),
-       loser integer references players(id));
+       winner integer REFERENCES players(id),
+       loser integer REFERENCES players(id));
 
-create view wins as 
-	select id, 
-		(select count(*) from matches where players.id = matches.winner) as wins 
-	from players;
+CREATE VIEW wins AS 
+	SELECT id, 
+		(SELECT count(*) FROM matches WHERE players.id = matches.winner) AS wins 
+	FROM players;
 
-create view opponentsList as
-		select id, loser as opponent
-		from players join matches
-		on id = winner
-	union
-		select id, winner as opponent
-		from players join matches
-		on id = loser
-	order by id, opponent;
+CREATE VIEW opponentsList AS
+		SELECT id, loser AS opponent
+		FROM players join matches
+		ON id = winner
+	UNION
+		SELECT id, winner AS opponent
+		FROM players join matches
+		ON id = loser
+	ORDER BY id, opponent;
 
-create view opponentsWinRank as
-	select opponentslist.id, sum(wins) as opponent_wins
-		from opponentslist join wins 
-		on opponent = wins.id 
-		group by opponentslist.id 
-		order by opponentslist.id;
+CREATE VIEW opponentsWinRank AS
+	SELECT opponentslist.id, sum(wins) AS opponent_wins
+		FROM opponentslist join wins 
+		ON opponent = wins.id 
+		GROUP BY opponentslist.id 
+		ORDER BY opponentslist.id;
 
 -- with help from http://discussions.udacity.com/t/p2-normalized-table-design/19927/2
-create view standings as
-	select players.id,name,
-		(select count(*) from matches where players.id = matches.winner) as wins,
-		(select count(*) from matches where players.id = matches.winner or players.id = matches.loser) as matches, 
-	opponent_wins as OMW
-from players left join opponentsWinRank on players.id = opponentswinrank.id
-order by wins desc, OMW desc, matches;
+CREATE VIEW standings AS
+	SELECT players.id,name,
+		(SELECT count(*) FROM matches WHERE players.id = matches.winner) AS wins,
+		(SELECT count(*) FROM matches WHERE players.id = matches.winner OR players.id = matches.loser) AS matches, 
+	opponent_wins AS OMW
+FROM players LEFT JOIN opponentsWinRank ON players.id = opponentswinrank.id
+ORDER BY wins DESC, OMW DESC, matches;
 
 -- adapted from http://stackoverflow.com/questions/19595809/skip-every-nth-result-row-in-postgresql
 -- Odd rows
-create view oddRows as
-	select o.id,o.name,o.wins,o.matches,row_number() over (order by wins desc, matches)
-	from (select id,name,wins,matches,row_number() over (order by wins desc, matches) as rank from standings) o 
-	where o.rank % 2 = 1; 
+CREATE VIEW oddRows AS
+	SELECT o.id,o.name,o.wins,o.matches,row_number() over (ORDER BY wins DESC, matches)
+	FROM (SELECT id,name,wins,matches,row_number() over (ORDER BY wins DESC, matches) AS rank FROM standings) o 
+	WHERE o.rank % 2 = 1; 
 -- Even rows
-create view evenRows as
-	select e.id,e.name,e.wins,e.matches,row_number() over (order by wins desc, matches)
-	from (select id,name,wins,matches,row_number() over (order by wins desc, matches) as rank from standings) e
-	where e.rank % 2 = 0; 
+CREATE VIEW evenRows AS
+	SELECT e.id,e.name,e.wins,e.matches,row_number() over (ORDER BY wins DESC, matches)
+	FROM (SELECT id,name,wins,matches,row_number() over (ORDER BY wins DESC, matches) AS rank FROM standings) e
+	WHERE e.rank % 2 = 0; 
 
-create view pairs as
-	select oddRows.id as id1, oddRows.name as name1, evenRows.id as id2, evenRows.name as name2
-	from oddRows join evenRows
-	on oddRows.row_number = evenRows.row_number;
+CREATE VIEW pairs AS
+	SELECT oddRows.id AS id1, oddRows.name AS name1, evenRows.id AS id2, evenRows.name AS name2
+	FROM oddRows join evenRows
+	ON oddRows.row_number = evenRows.row_number;
